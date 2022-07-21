f40cdcc415125bee638188ea4ca49d1bb397580c
JiheeSon <jihee.sonsational@gmail.com>
2022-07-20 23:59:02 -0500

    modifed splash screen

--- app/src/main/java/com/example/ecommerceapp/view/SplashScreen.kt
+++ app/src/main/java/com/example/ecommerceapp/view/SplashScreen.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import com.example.ecommerceapp.R
 import com.example.ecommerceapp.model.storage.getEncryptedPrefs
 import com.example.ecommerceapp.model.storage.isLoggedIn
 import com.example.ecommerceapp.view.activity.LoginActivity
@@ -15,11 +17,14 @@ class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
 
-        encryptedSharedPrefs = getEncryptedPrefs(this)
-        verifyLogin()
+        Handler().postDelayed({
+            encryptedSharedPrefs = getEncryptedPrefs(this)
+            verifyLogin()
+            finish()
+        }, 2000)
 
-        Log.i("tag", "splash screen")
     }
 
     private fun verifyLogin() {
